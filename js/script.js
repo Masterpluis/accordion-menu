934821f15a745d618256ea84cf0304095e3bd055
Masterpluis <jurriaan-flikkema@hotmail.com>
2022-02-02 16:00:16 +0100

    Converted accordion functionality from CSS to JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-const accordion = Array.from(document.getElementsByClassName('container'));
+// Select accordion items
+const accordion = Array.from(document.querySelectorAll(".container"));
 
+// Accordion event listener
 accordion.forEach(element => {
-	element.addEventListener('click', () => element.classList.toggle('active'))
+	element.addEventListener('click', () => accordionToggle(element))
 });
+
+// Unhides accordion content
+function accordionToggle(element) {
+	let elementContent = element.querySelector('.content');
+	element.classList.contains('active') ? elementContent.style.height = 0 : elementContent.style.height = elementContent.scrollHeight + 16 + 'px';
+	element.classList.toggle('active');
+}
